14e611a19e7dc6632c04c0e2c4c46c0504dbf340
whywidodo <mr.wahyuwidodo@gmail.com>
2023-12-11 20:31:13 +0700

    add: encrypt for order transaction id

--- utils/index.js
+++ utils/index.js
@@ -13,7 +13,20 @@ const imageKit = new ImageKit({
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
+const encryptOrder = async () => {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let orderTrx = "";
+
+  for (let i = 0; i < 5; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    orderTrx += characters.charAt(randomIndex);
+  }
+
+  return orderTrx;
+};
+
 module.exports = {
+  encryptOrder,
   encryptPassword,
   imageKit,
 };
