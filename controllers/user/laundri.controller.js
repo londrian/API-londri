05b606351c99623cea625a7a3e603c33dd8fe4ef
whywidodo <mr.wahyuwidodo@gmail.com>
2023-12-15 19:18:12 +0700

    add: search feature

--- controllers/user/laundri.controller.js
+++ controllers/user/laundri.controller.js
@@ -84,7 +84,59 @@ const detailLaundry = async (req, res) => {
   }
 };
 
+const searchLaundry = async (req, res) => {
+  try {
+    const laundrySearchParams = req.query.laundry;
+    const data = await laundry.findMany({
+      where: {
+        namaLaundry: {
+          contains: laundrySearchParams,
+          mode: "insensitive",
+        },
+      },
+      orderBy: {
+        id: "asc",
+      },
+      select: {
+        namaLaundry: true,
+        fotoLaundry: true,
+        nomorTelepon: true,
+        alamat: true,
+        status: true,
+        layanan: {
+          select: {
+            id: true,
+            namaLayanan: true,
+            status: true,
+            hargaLayanan: true,
+          },
+        },
+      },
+    });
+
+    if (data == null) {
+      return res.status(200).json({
+        error: false,
+        message: "Data laundri tidak ditemukan",
+      });
+    }
+
+    return res.status(200).json({
+      error: false,
+      message: "Data laundri berhasil ditemukan",
+      resultData: data,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: true,
+      message: error || "Internal server error",
+    });
+  }
+};
+
 module.exports = {
   allLaundry,
   detailLaundry,
+  searchLaundry,
 };
--- routers/user/laundri.route.js
+++ routers/user/laundri.route.js
@@ -4,5 +4,6 @@ const controller = require("../../controllers/user/laundri.controller");
 
 router.get("/", controller.allLaundry);
 router.get("/detail/:laundryId", controller.detailLaundry);
+router.get("/search", controller.searchLaundry);
 
 module.exports = router;
